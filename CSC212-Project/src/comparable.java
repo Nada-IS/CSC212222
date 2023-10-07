780978e91ad936fd47b19f8087e7a43ad0fe6fe4
nora221 <noraalsaif2003@gmail.com>
2023-10-07 20:13:38 +0300

    delete

--- CSC212-Project/src/comparable.java
+++ CSC212-Project/src/comparable.java
@@ -5,7 +5,5 @@ public interface  comparable <T>{
 <<<<<<< HEAD
 	System.out.print();
 =======
-	System.out.print("hhd");
-	System.out.print("jjj");
 >>>>>>> branch 'main' of https://github.com/Nada-IS/CSC212222.git
 }
