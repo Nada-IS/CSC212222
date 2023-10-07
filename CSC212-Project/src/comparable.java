e8b8a720f33d65c7fe9b15442373287d07040a56
nora221 <noraalsaif2003@gmail.com>
2023-10-07 20:14:50 +0300

    delete

--- CSC212-Project/src/comparable.java
+++ CSC212-Project/src/comparable.java
@@ -2,8 +2,5 @@
 public interface  comparable <T>{
 	
 	public int compareTo(Contact name);
-<<<<<<< HEAD
-	System.out.print();
-=======
->>>>>>> branch 'main' of https://github.com/Nada-IS/CSC212222.git
+
 }
